cd03a60ab004cb6e745d5df2a735837f6c753b5a
BloodFalka <bloodfallka@gmail.com>
2020-04-14 19:39:26 +0300

    postFORMS

--- dist/js/script.js
+++ dist/js/script.js
@@ -91,29 +91,29 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', deadline);
 
-    //Modal
+    //Modal post
     let more = document.querySelector(`.more`),
         infoBlock = document.querySelector(`.info`),
         infoMore = document.querySelectorAll(`.description-btn`),
         overlay = document.querySelector(`.overlay`),
         close = document.querySelector(`.popup-close`);
 
-    more.addEventListener(`click`, function(){
+    more.addEventListener(`click`, function() {
         overlay.style.display = `block`;
         this.classList.add(`more-splash`);
         document.body.style.overflow = `hidden`;
     });
 
-    infoBlock.addEventListener(`click`,function(event){
+    infoBlock.addEventListener(`click`, function(event) {
         let target = event.target;
-        if (target && target.classList.contains(`description-btn`)){
+        if (target && target.classList.contains(`description-btn`)) {
             overlay.style.display = `block`;
             target.classList.add(`more-splash`);
-            document.body.style.overflow = `hidden`;   
+            document.body.style.overflow = `hidden`;
         }
     });
 
-    close.addEventListener(`click`, function(){
+    close.addEventListener(`click`, function() {
         overlay.style.display = `none`;
         more.classList.remove(`more-splash`);
         document.body.style.overflow = `visible`;
@@ -124,4 +124,57 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //            FORMS POST
+    let message = {
+        loading: `Загрузка...`,
+        success: `Спасибо, скоро свяжемся с вами`,
+        failure: `Что-то пошло не так`,
+    };
+
+    //Form POST
+    function postForm(formQuerySelector) {
+        let form = document.querySelector(formQuerySelector),
+            input = form.querySelectorAll(`input`),
+            statusMessage = document.createElement(`div`);
+
+        statusMessage.classList.add(`status`);
+
+        form.addEventListener(`submit`, function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener(`readystatechange`, function() {
+                if (request.readyState < 4) {
+                    statusMessage.textContent = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = ``;
+            }
+        });
+    }
+    postForm(`.main-form`);
+
+    postForm(`#form`);
+
 });
