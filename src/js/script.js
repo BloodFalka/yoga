8f9f4272fde1e61c1a1f4334080f25c5a3b59a19
BloodFalka <57503894+BloodFalka@users.noreply.github.com>
2020-04-20 11:58:21 +0300

    promise

--- src/js/script.js
+++ src/js/script.js
@@ -144,35 +144,47 @@ window.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             form.appendChild(statusMessage);
 
-            let request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
             let formData = new FormData(form);
-
+            
             let obj = {};
             formData.forEach(function(value, key) {
                 obj[key] = value;
             });
             let json = JSON.stringify(obj);
 
-            request.send(json);
+            function sendData(data){
+                return new Promise(function(resolve, reject){    
+                    let request = new XMLHttpRequest();
 
-            request.addEventListener(`readystatechange`, function() {
-                if (request.readyState < 4) {
-                    statusMessage.textContent = message.loading;
-                } else if (request.readyState === 4 && request.status == 200) {
-                    statusMessage.textContent = message.success;
-                } else {
-                    statusMessage.textContent = message.failure;
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                    request.addEventListener(`readystatechange`, function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    })
+                    request.send(data);
+                })
+            };
+
+            function deleteInputs(){
+              for (let i = 0; i < input.length; i++) {
+                    input[i].value = ``;
                 }
-            });
-
-            for (let i = 0; i < input.length; i++) {
-                input[i].value = ``;
             }
+            sendData(json)
+                .then(()=> statusMessage.textContent = message.loading)
+                .then(()=> statusMessage.textContent = message.success)
+                .catch(()=> statusMessage.textContent = message.failure)
+                .then(()=> deleteInputs())
         });
     }
+
     postForm(`.main-form`);
 
     postForm(`#form`);
