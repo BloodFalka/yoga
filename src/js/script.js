b3e2ab30ca4c35660600c5574d523ba7a478ee23
BloodFalka <57503894+BloodFalka@users.noreply.github.com>
2020-04-22 12:12:18 +0300

    complete calculator

--- src/js/script.js
+++ src/js/script.js
@@ -232,4 +232,50 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Calculator
+    let persons = document.querySelectorAll(`.counter-block-input`)[0],
+        days = document.querySelectorAll(`.counter-block-input`)[1],
+        location = document.querySelector(`#select`),
+        totalSum = document.querySelector(`#total`),
+
+        personsValue = 0,
+        daysValue = 0,
+        locationRate = 0,
+        totalValue = 0;
+
+    totalSum.textContent = "0";
+
+    persons.addEventListener('input', function(){
+        personsValue = +this.value;
+        totalValue = personsValue*daysValue*2000*location.options[location.selectedIndex].value;
+   
+        if(days.value == '' || location.options[location.selectedIndex].value == ''){
+            totalSum.textContent = "0"
+        } else {
+            totalSum.textContent = totalValue
+        }
+    });
+    
+    days.addEventListener('input', function(){
+        daysValue = +this.value;
+        totalValue = personsValue*daysValue*2000*location.options[location.selectedIndex].value;
+
+        if(location.options[location.selectedIndex].value == '' || persons.value == ''){
+            totalSum.textContent = "0"
+        } else {
+            totalSum.textContent = totalValue
+        }
+    });
+
+    location.addEventListener('input', function(){
+        totalValue = personsValue*daysValue*2000*location.options[location.selectedIndex].value;
+
+        if(days.value == '' || persons.value == ''){
+            totalSum.textContent = "0"
+        } else {
+            totalSum.textContent = totalValue
+        }
+    });
+
 });
